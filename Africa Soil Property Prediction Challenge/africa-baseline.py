35ab44737bf0af0abfd45c48c4693b93d045ad19
Rupak Chakraborty <rupachak@adobe.com>
2017-11-14 12:15:49 +0530

    Added Africa Soil Property Dataset

---
+++ Africa Soil Property Prediction Challenge/africa-baseline.py
@@ -0,0 +1,87 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.ensemble import BaggingRegressor
+from sklearn.ensemble import ExtraTreesRegressor
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import RandomTreesEmbedding
+from sklearn.neural_network import MLPRegressor
+from sklearn.linear_model import ElasticNet
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.multioutput import MultiOutputRegressor
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import Imputer
+from sklearn import metrics
+import numpy as np
+
+
+def get_multioutput_regressor(base_estimator):
+    multi = MultiOutputRegressor(base_estimator)
+    return multi
+
+
+def get_gaussian_process_regressor():
+    gp = GaussianProcessRegressor()
+    return [gp],['Gaussian Process']
+
+
+def get_mlp_regressor(num_hidden_units=51):
+    mlp = MLPRegressor(hidden_layer_sizes=num_hidden_units)
+    return [mlp],['Multi-Layer Perceptron']
+
+
+def get_ensemble_models():
+    rf = RandomForestRegressor(n_estimators=51,min_samples_leaf=5,min_samples_split=3,random_state=42)
+    bag = BaggingRegressor(n_estimators=51,random_state=42)
+    extra = ExtraTreesRegressor(n_estimators=71,random_state=42)
+    ada = AdaBoostRegressor(random_state=42)
+    grad = GradientBoostingRegressor(n_estimators=101,random_state=42)
+    classifier_list = [rf,bag,extra,ada,grad]
+    classifier_name_list = ['Random Forests','Bagging','Extra Trees','AdaBoost','Gradient Boost']
+    return classifier_list, classifier_name_list
+
+
+def get_linear_model():
+    elastic_net = ElasticNet()
+    return [elastic_net],['Elastic Net']
+
+
+def print_evaluation_metrics(trained_model,trained_model_name,X_test,y_test):
+    print '--------- For Model : ', trained_model_name ,' ---------\n'
+    print "R2 Score : ", trained_model.score(X_test,y_test)
+    print "---------------------------------------\n"
+
+
+def label_encode_frame(dataframe):
+    columns = dataframe.columns
+    encoder = LabelEncoder()
+    for column in columns:
+        if type(dataframe[column][0]) is np.nan:
+            for i in range(len(dataframe)):
+                if i > 1000:
+                    break
+                if type(dataframe[column][i]) is str:
+                    dataframe[column] = encoder.fit_transform(dataframe[column].values)
+                    break
+        elif type(dataframe[column][0]) is str:
+            dataframe[column] = encoder.fit_transform(dataframe[column].values)
+    return dataframe
+
+
+filename = 'training.csv'
+train_frame = pd.read_csv(filename)
+output_columns = [ 'Ca','P','pH','SOC','Sand']
+output_labels = train_frame[output_columns].values
+train_frame.drop(output_columns,axis=1,inplace=True)
+train_frame = label_encode_frame(train_frame)
+del train_frame['PIDN']
+
+X_train,X_test,y_train,y_test = train_test_split(train_frame.values,output_labels,test_size=0.1,random_state=42)
+classifier_list, classifier_name_list = get_ensemble_models()
+for classifier,classifier_name in zip(classifier_list,classifier_name_list):
+    multi_regressor = get_multioutput_regressor(classifier)
+    multi_regressor.fit(X_train,y_train)
+    print_evaluation_metrics(multi_regressor,classifier_name,X_test,y_test)
+
+
